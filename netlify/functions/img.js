0eb4dc9ec278dba2302d324fcbeb76b4bb0e6593
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2021-11-27 14:25:50 +0100

    setup on demand builder function for images

--- netlify/functions/img.js
+++ netlify/functions/img.js
@@ -1,63 +1,48 @@
-// const { builder } = require("@netlify/functions");
-const fetch = require('node-fetch');
+const { builder } = require('@netlify/functions');
+const fetch = require('node-fetch').default;
 const API_ROOT = process.env.VITE_MEDIA_URL;
-
-
-exports.handler = async function (event, context) {
-  const pieces = event.path.split('===');
-
-  try {
-    const res = await fetch(`${API_ROOT}${pieces[0]}`)
-  } catch (e) {
-    console.log(e);
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-  };
-}
 
 /**
  request (query params don't work)
- http://localhost:8888/images/media/nc5dhsxb/eiffel_tower_vertical.jpg===width/300
- event: {
-  path: "/images/media/nc5dhsxb/eiffel_tower_vertical.jpg===width/300"
-}
-
- can use: process.env.VITE_MEDIA_URL
+ http://localhost:8888/img/media/nc5dhsxb/eiffel_tower_vertical.jpg===width/300
  **/
 
-/*
 async function handler(event, context) {
+  const prefix = '/img'; //need to be stripped of the path
   const pieces = event.path.split('===');
-  // const {
-  //   img,
-  //   params,
-  // } = event.path;
+  // turn `width/300/height/300` into `?width=300&height=300
+  let isValue = false; // it's the property (width)
+  let qs = '?quality=30&mode=crop';
+  pieces[1].split('/').forEach((param) => {
+    if (!isValue) { // i.e. 'width'
+      qs += `&${param}=`;
+      isValue = true;
+    } else {
+      qs += `${param}`;
+      isValue = false;
+    }
+  });
+  const url = `${API_ROOT}${pieces[0].substring(prefix.length)}${qs}`;
 
   try {
-    const res = await fetch(`${API_ROOT}${pieces[0]}`)
+    const req = await fetch(url);
+    const res = await req.arrayBuffer();
+    return {
+      statusCode: 200,
+      body: new Buffer(res).toString('base64'),
+      isBase64Encoded: true,
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+    };
   } catch (e) {
     console.log(e);
-  }
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(pieces)
-    // body: `
-    // <!DOCTYPE html>
-	  //   <html>
-		//     <body>
-		//       Hello World
-		//     </body>
-    // </html>
-    // `,
-  };
+    return {
+      statusCode: 500,
+      body: `error: ${e}`,
+    };
+  }
 }
 
 exports.handler = builder(handler);
-*/
