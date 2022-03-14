4da62c5645ba89d846dbb29f6bf2e3de111b7df1
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2022-03-14 12:35:10 +0100

    add different image formats

--- src/components/atoms/objects/Img.svelte
+++ src/components/atoms/objects/Img.svelte
@@ -13,18 +13,43 @@
   }
 
   let srcset = [];
-  if (sizes) {
-    srcset = sizes.map((size) => `${path}/width:${size}::${url} ${size}w`);
-  }
+  // if (sizes) {
+  //   srcset = sizes.map((size) => `${path}/width:${size}/format:avif::${url} ${size}w`);
+  // }
+
+  const getSrcset = ({ format = 'jpg' }) => {
+    // console.log(format);
+    if (sizes) {
+      srcset = sizes.map((size) => `${path}/width:${size}/format:${format}::${url} ${size}w`);
+    }
+
+    return srcset;
+  };
 </script>
 
-<img
-  alt={name}
-  class="lazyload"
-  data-srcset={lazy ? `${srcset.join(',')}` : null}
-  src=""
-  width="{width}"
-  height="{height}"
-  sizes="auto"
-  srcset={!lazy ? `${srcset.join(',')}` : null}
-/>
+<picture>
+  <source
+    data-srcset={lazy ? getSrcset({ format: 'avif' }) : null}
+    srcset={!lazy ? getSrcset({ format: 'avif' }) : null}
+    type="image/avif"
+  >
+  <source
+    data-srcset={lazy ? getSrcset({ format: 'webp' }) : null}
+    srcset={!lazy ? getSrcset({ format: 'webp' }) : null}
+    type="image/webp"
+  >
+  <source
+    data-srcset={lazy ? getSrcset({ format: 'jpg' }) : null}
+    srcset={!lazy ? getSrcset({ format: 'jpg' }) : null}
+  >
+  <img
+    alt={name}
+    class="lazyload"
+    height="{height}"
+    sizes="auto"
+    src=""
+    width="{width}"
+  />
+</picture>
+
+
