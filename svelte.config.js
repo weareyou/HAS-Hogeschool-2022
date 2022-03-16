493920fcf6a2ab5c3c8d483895e4408640490207
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2022-03-16 09:15:57 +0100

    add build option for preview

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import sveltePreprocess from 'svelte-preprocess';
-import netlifyAdapter from '@sveltejs/adapter-netlify';
+import adapterNetlify from '@sveltejs/adapter-netlify';
+import adapterStatic from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,14 +8,14 @@ const config = {
     preserve: ['ld+json'],
   }),
   kit: {
-    // prerender: {
-    //   default: true,
-    // },
+    prerender: {
+      default: true,
+    },
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    // adapter: adapter(),
-    adapter: netlifyAdapter(),
+    // adapter: adapterStatic(),
+    adapter: process.env.npm_config_adapter === 'netlify' ? adapterNetlify() : adapterStatic(),
 
     // trailingSlash: 'never',
     // csp: {
